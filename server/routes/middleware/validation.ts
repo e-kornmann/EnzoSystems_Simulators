91a8e4d1371dc71e0606714202226087f07ad26a
Elton Kornmann <elton.kornmann@gmail.com>
2023-07-10 09:27:28 +0200

    put some logic in the middleware

--- server/routes/middleware/validation.ts
+++ server/routes/middleware/validation.ts
@@ -4,25 +4,86 @@ import { PaymentData } from '../payment_terminal';
 const isIdle = (req: Request, res: Response, next: NextFunction) => {
     
     if (PaymentData.state !== 'idle') {
-         return res.status(400).send('Another payment is already in progress.');
+         return res
+           .status(400)
+           .send('Another payment is already in progress.');
      }
     next();
    }
  
  
-const validatePaymentRequest = (req: Request, res: Response, next: NextFunction) => {
+const validatePaymentPostRequest = (req: Request, res: Response, next: NextFunction) => {
     const { amount } = req.body;   
     if (amount === undefined || amount === null || isNaN(amount)) {
-      return res.status(400).send('Please provide a valid numeric amount for the payment.');
+      return res
+        .status(400)
+        .send('Please provide a valid numeric amount for the payment.');
     }
     if (amount === 0) {
-      return res.status(400).send('Please provide a non-zero amount for the payment.');
+      return res
+        .status(400)
+        .send('Please provide a non-zero amount for the payment.');
     }
     next();
   }
 
+  const validatePaymentGetRequest = async (req: Request, res: Response, next: NextFunction) => {
+    const { terminalId, transactionId } = req.params;
+  
+    if (transactionId !== PaymentData.transactionId || terminalId !== PaymentData.terminalId) {
+      return res
+        .status(404)
+        .send('Transaction not found');
+    }
+    next();
+  };
 
-export { isIdle,validatePaymentRequest } ;
+
+
+  const checkPaymentStatus = async (req: Request, res: Response, next: NextFunction) => {
+    const timeout = 30000;
+    const startTime = Date.now();
+  
+    const intervalId = setInterval(() => {
+      switch (PaymentData.state) {
+        case 'running':
+          if ((Date.now() - startTime) >= timeout) {
+            clearInterval(intervalId);
+            req.body = {
+              state: 'timedout',
+              amountPaid: PaymentData.amountPaid,
+              receipt: 'Transaction timed-out',
+            };
+            next();
+          }
+          break;
+        case 'finished':
+          clearInterval(intervalId);
+          req.body = {
+            state: PaymentData.state,
+            amountPaid: PaymentData.amountPaid,
+            receipt: 'You have paid',
+          };
+          next();
+          break;
+        case 'stopped':
+          clearInterval(intervalId);
+          req.body = {
+            state: PaymentData.state,
+            amountPaid: PaymentData.amountPaid,
+            receipt: 'Transaction stopped',
+          };
+          next();
+          break;
+        default:
+          clearInterval(intervalId);
+          return res.status(500).send('An error occurred');
+      }
+    }, 1000);
+  };
+  
+  export { isIdle, validatePaymentPostRequest, validatePaymentGetRequest, checkPaymentStatus };
+
 
 
 
--- server/routes/payment_terminal.ts
+++ server/routes/payment_terminal.ts
@@ -1,6 +1,6 @@
 import Router, { Response, Request } from 'express';
 import crypto from 'crypto';
-import { validatePaymentRequest, isIdle } from './middleware/validation';
+import { validatePaymentPostRequest, validatePaymentGetRequest, isIdle, checkPaymentStatus } from './middleware/validation';
 
 const payment_terminal = Router();
 
@@ -17,9 +17,7 @@ export const PaymentData = {
   receipt: null,
 };
 
-console.log(PaymentData);
-
-payment_terminal.post('/:terminalId/payment', isIdle, validatePaymentRequest,
+payment_terminal.post('/:terminalId/payment', isIdle, validatePaymentPostRequest,
   async (req: Request, res: Response) => {
     const { terminalId } = req.params;
     const { amount } = req.body;
@@ -30,7 +28,6 @@ payment_terminal.post('/:terminalId/payment', isIdle, validatePaymentRequest,
     PaymentData.amountPaid = 0;
     PaymentData.receipt = null;
     try {
-
       res.status(200).json(PaymentData);
     } catch (error) {
       console.error(error);
@@ -39,42 +36,9 @@ payment_terminal.post('/:terminalId/payment', isIdle, validatePaymentRequest,
   }
 );
 
-payment_terminal.get('/:terminalId/payment/:transactionId', async (req, res) => {
-  const { terminalId, transactionId } = req.params;
-
-  if (transactionId !== PaymentData.transactionId || terminalId !== PaymentData.terminalId) {
-    return res.status(404).send('Transaction not found');
-  }
-
-  const timeout = 30000; // Timeout in milliseconds (e.g., 30 seconds)
-  const startTime = Date.now();
-
-  while (PaymentData.state === 'running' && (Date.now() - startTime) < timeout) {
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second before checking the state again
-  }
-
-  if (PaymentData.state === 'finished') {
-    return res.status(200).json({
-      state: PaymentData.state,
-      amountPaid: PaymentData.amountPaid,
-      receipt: 'You have paid',
-    });
-  } else if (PaymentData.state === 'stopped') {
-    return res.status(200).json({
-      state: PaymentData.state,
-      amountPaid: PaymentData.amountPaid,
-      receipt: 'Transaction stopped',
-    });
-  } else if (PaymentData.state === 'timedout') {
-    return res.status(200).json({
-      state: PaymentData.state,
-      amountPaid: PaymentData.amountPaid,
-      receipt: 'Transaction timed-out',
-    });
-  } else {
-    return res.status(500).send('An error occurred');
-  }
-});
-
+payment_terminal.get('/:terminalId/payment/:transactionId', validatePaymentGetRequest, checkPaymentStatus, 
+  async (req: Request, res: Response) => {
+    res.status(200).json(req.body);
+  });
 
 export default payment_terminal;
