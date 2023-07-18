72f11071e97fdee4608a495374a8f8f980524f27
Elton Kornmann <elton.kornmann@gmail.com>
2023-07-18 18:28:50 +0200

    inititalize redux tool kit

--- client/src/components/simulators/PaymentDevice/Personalisation/AppSettings/AvailableSettings/OperationalModeOptions.tsx
+++ client/src/components/simulators/PaymentDevice/Personalisation/AppSettings/AvailableSettings/OperationalModeOptions.tsx
@@ -1,15 +1,36 @@
-
 import { ReactComponent as Arrow } from '../../../../../../assets/svgs/arrow.svg';
+import { AllAppSettings, OperationalModeOptionsStates } from '../../../../../../store/reducers/settings.slice';
 import { Button, List } from "../../style";
+import { useSelector } from 'react-redux';
 
-const OperationalModeOptions = () => 
+const OperationalModeOptions = () => {
+  const settings: AllAppSettings = useSelector((state: any) => state.settings);
+  const { operationalModeOptions } = settings;
 
-<List>
-  <Button>Normal<Arrow/></Button>
-  <Button>Always succeed<Arrow/></Button>
-  <Button>Always fail<Arrow/></Button>
-  <Button>First fail<Arrow/></Button>
-  <Button>First fail, then succeed<Arrow/></Button>
-</List>
+  return (
+    <List>
+      <Button>
+        Normal
+        <input type="radio" id='normal' value="normal" checked={operationalModeOptions === OperationalModeOptionsStates.NORMAL} />
+      </Button>
+      <Button>
+        Always succeed
+        <Arrow />
+      </Button>
+      <Button>
+        Always fail
+        <Arrow />
+      </Button>
+      <Button>
+        First fail
+        <Arrow />
+      </Button>
+      <Button>
+        First fail, then succeed
+        <Arrow />
+      </Button>
+    </List>
+  );
+};
 
 export default OperationalModeOptions;
